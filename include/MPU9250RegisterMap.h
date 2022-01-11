922bf45f047d2ee04eda5bf730411e1f1fed5249
Konstantinos Karatzidis <kostaskarat@hotmail.com>
2022-01-11 17:47:34 +0200

    Representing bit fields as hex

--- include/MPU9250RegisterMap.h
+++ include/MPU9250RegisterMap.h
@@ -10,37 +10,37 @@
 
 // Power Management 1 (107)
 #define PWR_MGMT_1               0x6B
-#define PWR_MGMT_1_H_RESET       (1 << 7)
-#define PWR_MGMT_1_SLEEP         (1 << 6)
-#define PWR_MGMT_1_CYCLE         (1 << 5)
-#define PWR_MGMT_1_GYRO_STANDBY  (1 << 4)
-#define PWR_MGMT_1_PD_PTAT       (1 << 3)
+#define PWR_MGMT_1_H_RESET       (0x80)
+#define PWR_MGMT_1_SLEEP         (0x40)
+#define PWR_MGMT_1_CYCLE         (0x20)
+#define PWR_MGMT_1_GYRO_STANDBY  (0x10)
+#define PWR_MGMT_1_PD_PTAT       (0x08)
 #define PWR_MGMT_1_CLKSEL(s)     ((s) & 0b111)
 #define PWR_MGMT_1_CLKSEL_MASK   (0b111)
 
 // Power Management 2 (108)
 #define PWR_MGMT_2             0x6C
-#define PWR_MGMT_2_DISABLE_XA  (1 << 5)
-#define PWR_MGMT_2_DISABLE_YA  (1 << 4)
-#define PWR_MGMT_2_DISABLE_ZA  (1 << 3)
-#define PWR_MGMT_2_DISABLE_XG  (1 << 2)
-#define PWR_MGMT_2_DISABLE_YG  (1 << 1)
-#define PWR_MGMT_2_DISABLE_ZG  (1 << 0)
+#define PWR_MGMT_2_DISABLE_XA  (0x20)
+#define PWR_MGMT_2_DISABLE_YA  (0x10)
+#define PWR_MGMT_2_DISABLE_ZA  (0x08)
+#define PWR_MGMT_2_DISABLE_XG  (0x04)
+#define PWR_MGMT_2_DISABLE_YG  (0x02)
+#define PWR_MGMT_2_DISABLE_ZG  (0x01)
 
 // Signal Path Reset (120)
 #define SIGNAL_PATH_RESET        0x68
-#define SIGNAL_PATH_RESET_GYRO   (1 << 2)
-#define SIGNAL_PATH_RESET_ACCEL  (1 << 1)
-#define SIGNAL_PATH_RESET_TEMP   (1 << 0)
+#define SIGNAL_PATH_RESET_GYRO   (0x04)
+#define SIGNAL_PATH_RESET_ACCEL  (0x02)
+#define SIGNAL_PATH_RESET_TEMP   (0x01)
 
 // User Control (106)
 #define USER_CTRL               0x6A
-#define USER_CTRL_FIFO_EN       (1 << 6)
-#define USER_CTRL_I2C_MST_EN    (1 << 5)
-#define USER_CTRL_I2C_IF_DIS    (1 << 4)
-#define USER_CTRL_FIFO_RST      (1 << 2)
-#define USER_CTRL_I2C_MST_RST   (1 << 1)
-#define USER_CTRL_SIG_COND_RST  (1 << 0)
+#define USER_CTRL_FIFO_EN       (0x40)
+#define USER_CTRL_I2C_MST_EN    (0x20)
+#define USER_CTRL_I2C_IF_DIS    (0x10)
+#define USER_CTRL_FIFO_RST      (0x04)
+#define USER_CTRL_I2C_MST_RST   (0x02)
+#define USER_CTRL_SIG_COND_RST  (0x01)
 
 
 ////////////////////////////////////////////////////
@@ -49,36 +49,36 @@
 
 // INT Pin / Bypass Enable Configuration (55)
 #define INT_PIN_CFG                    0x37
-#define INT_PIN_CFG_ACTL               (1 << 7)
-#define INT_PIN_CFG_OPEN               (1 << 6)
-#define INT_PIN_CFG_LATCH_INT_EN       (1 << 5)
-#define INT_PIN_CFG_ANYRD_2CLEAR       (1 << 4)
-#define INT_PIN_CFG_ACTL_FSYNC         (1 << 3)
-#define INT_PIN_CFG_FSYNC_INT_MODE_EN  (1 << 2)
-#define INT_PIN_CFG_BYPASS_EN          (1 << 1)
+#define INT_PIN_CFG_ACTL               (0x80)
+#define INT_PIN_CFG_OPEN               (0x40)
+#define INT_PIN_CFG_LATCH_INT_EN       (0x20)
+#define INT_PIN_CFG_ANYRD_2CLEAR       (0x10)
+#define INT_PIN_CFG_ACTL_FSYNC         (0x08)
+#define INT_PIN_CFG_FSYNC_INT_MODE_EN  (0x04)
+#define INT_PIN_CFG_BYPASS_EN          (0x02)
 
 // Interrupt Enable (56)
 #define INT_ENABLE                0x38
-#define INT_ENABLE_WOM            (1 << 6)
-#define INT_ENABLE_FIFO_OVERFLOW  (1 << 4)
-#define INT_ENABLE_FSYNC          (1 << 3)
-#define INT_ENABLE_RAW_RDY        (1 << 0)
+#define INT_ENABLE_WOM            (0x40)
+#define INT_ENABLE_FIFO_OVERFLOW  (0x10)
+#define INT_ENABLE_FSYNC          (0x08)
+#define INT_ENABLE_RAW_RDY        (0x01)
 
 // Interrupt Status (58)
 #define INT_STATUS                0x3A
-#define INT_STATUS_WOM            (1 << 6)
-#define INT_STATUS_FIFO_OVERFLOW  (1 << 4)
-#define INT_STATUS_FSYNC          (1 << 3)
-#define INT_STATUS_RAW_RDY        (1 << 0)
+#define INT_STATUS_WOM            (0x40)
+#define INT_STATUS_FIFO_OVERFLOW  (0x10)
+#define INT_STATUS_FSYNC          (0x08)
+#define INT_STATUS_RAW_RDY        (0x01)
 
 // Accelerometer Interrupt Control (105)
 // (the same register as MOT_DETECT_CTRL)
 #define MOT_DETECT_CTRL       0x69
-#define MOT_DETECT_CTRL_EN   (1 << 7)
-#define MOT_DETECT_CTRL_MODE (1 << 6)
+#define MOT_DETECT_CTRL_EN   (0x80)
+#define MOT_DETECT_CTRL_MODE (0x40)
 #define ACCEL_INTEL_CTRL      0x69
-#define ACCEL_INTEL_CTRL_EN   (1 << 7)
-#define ACCEL_INTER_CTRL_MODE (1 << 6)
+#define ACCEL_INTEL_CTRL_EN   (0x80)
+#define ACCEL_INTER_CTRL_MODE (0x40)
 
 // Wake-on Motion Threshold (31)
 #define WOM_THR          0x1F
@@ -93,7 +93,7 @@
 
 // Configuration (also affects gyroscope) (26)
 #define MPU_CONFIG                0x1A
-#define MPU_CONFIG_FIFO_MODE      (1 << 6)
+#define MPU_CONFIG_FIFO_MODE      (0x40)
 #define MPU_CONFIG_SYNC_SET(s)    (((s) & 0b111) << 3)
 #define MPU_CONFIG_SYNC_SET_MASK  (0b111 < 3)
 #define MPU_CONFIG_DLPF_CFG(s)    ((s) & 0b111)
@@ -105,9 +105,9 @@
 
 // Gyroscope Configuration (27)
 #define GYRO_CONFIG                 0x1B
-#define GYRO_CONFIG_XGYRO_Cten      (1 << 7)
-#define GYRO_CONFIG_YGYRO_Cten      (1 << 6)
-#define GYRO_CONFIG_ZGYRO_Cten      (1 << 5)
+#define GYRO_CONFIG_XGYRO_Cten      (0x80)
+#define GYRO_CONFIG_YGYRO_Cten      (0x40)
+#define GYRO_CONFIG_ZGYRO_Cten      (0x20)
 #define GYRO_CONFIG_FS_SEL(s)       (((s) & 0b11) << 3)
 #define GYRO_CONFIG_FS_SEL_MASK     (0b11 << 3)
 #define GYRO_CONFIG_Fchoice_b(s)    ((s) & 0b11)
@@ -132,15 +132,15 @@
 
 // Accelerometer Configuration (28)
 #define ACCEL_CONFIG              0x1C
-#define ACCEL_CONFIG_ax_st_en     (1 << 7)
-#define ACCEL_CONFIG_ay_st_en     (1 << 6)
-#define ACCEL_CONFIG_az_st_en     (1 << 5)
+#define ACCEL_CONFIG_ax_st_en     (0x80)
+#define ACCEL_CONFIG_ay_st_en     (0x40)
+#define ACCEL_CONFIG_az_st_en     (0x20)
 #define ACCEL_CONFIG_FS_SEL(s)    (((s) & 0b11) << 3)
-#define ACCEL_CONFIG_FS_SEL_MAKS  (0b11 << 3)
+#define ACCEL_CONFIG_FS_SEL_MASK  (0b11 << 3)
 
 // Accelerometer Configuration 2 (29)
 #define ACCEL_CONFIG2               0x1D
-#define ACCEL_CONFIG2_fchoice_b     (1 << 3)
+#define ACCEL_CONFIG2_fchoice_b     (0x08)
 #define ACCEL_CONFIG2_DLPFCFG(s)    (((s) & 0b111))
 #define ACCEL_CONFIG2_DLPFCFG_MASK  (0b111)
 
@@ -168,14 +168,14 @@
 
 // FIFO Enable (35)
 #define FIFO_EN        0x23
-#define FIFO_EN_TEMP   (1 << 7)
-#define FIFO_EN_GYROX  (1 << 6)
-#define FIFO_EN_GYROY  (1 << 5)
-#define FIFO_EN_GYROZ  (1 << 4)
-#define FIFO_EN_ACCEL  (1 << 3)
-#define FIFO_EN_SLV_2  (1 << 2)
-#define FIFO_EN_SLV_1  (1 << 1)
-#define FIFO_EN_SLV_0  (1 << 0)
+#define FIFO_EN_TEMP   (0x80)
+#define FIFO_EN_GYROX  (0x40)
+#define FIFO_EN_GYROY  (0x20)
+#define FIFO_EN_GYROZ  (0x10)
+#define FIFO_EN_ACCEL  (0x08)
+#define FIFO_EN_SLV_2  (0x04)
+#define FIFO_EN_SLV_1  (0x02)
+#define FIFO_EN_SLV_0  (0x01)
 
 // Fifo Count (114, 115)
 #define FIFO_COUNTH      0x72
@@ -191,47 +191,47 @@
 
 // I2C Master Control (36)
 #define I2C_MST_CTRL                0x24
-#define I2C_MST_CTRL_MULT_MST_EN    (1 << 7)
-#define I2C_MST_CTRL_WAIT_FOR_ES    (1 << 6)
-#define I2C_MST_CTRL_SLV_3_FIFO_EN  (1 << 5)
-#define I2C_MST_CTRL_P_NSR          (1 << 4)
+#define I2C_MST_CTRL_MULT_MST_EN    (0x80)
+#define I2C_MST_CTRL_WAIT_FOR_ES    (0x40)
+#define I2C_MST_CTRL_SLV_3_FIFO_EN  (0x20)
+#define I2C_MST_CTRL_P_NSR          (0x10)
 #define I2C_MST_CTRL_CLK(s)         ((s) & 0b1111)
 #define I2C_MST_CTRL_CLK_MASK       (0b1111)
 
 // I2C Master Delay Control (103)
 #define I2C_MST_DELAY_CTRL         0x67
-#define I2C_MST_DELAY_CTRL_SHADOW  (1 << 7)
-#define I2C_MST_DELAY_CTRL_SLV4    (1 << 4)
-#define I2C_MST_DELAY_CTRL_SLV3    (1 << 3)
-#define I2C_MST_DELAY_CTRL_SLV2    (1 << 2)
-#define I2C_MST_DELAY_CTRL_SLV1    (1 << 1)
-#define I2C_MST_DELAY_CTRL_SLV0    (1 << 0)
+#define I2C_MST_DELAY_CTRL_SHADOW  (0x80)
+#define I2C_MST_DELAY_CTRL_SLV4    (0x10)
+#define I2C_MST_DELAY_CTRL_SLV3    (0x08)
+#define I2C_MST_DELAY_CTRL_SLV2    (0x04)
+#define I2C_MST_DELAY_CTRL_SLV1    (0x02)
+#define I2C_MST_DELAY_CTRL_SLV0    (0x01)
 
 // I2C Master Status (54)
 #define I2C_MST_STATUS               0x36
-#define I2C_MST_STATUS_PASS_THROUGH  (1 << 7)
-#define I2C_MST_STATUS_SLV4_DONE     (1 << 6)
-#define I2C_MST_STATUS_LOST_ARB      (1 << 5)
-#define I2C_MST_STATUS_SLV4_NACK     (1 << 4)
-#define I2C_MST_STATUS_SLV3_NACK     (1 << 3)
-#define I2C_MST_STATUS_SLV2_NACK     (1 << 2)
-#define I2C_MST_STATUS_SLV1_NACK     (1 << 1)
-#define I2C_MST_STATUS_SLV0_NACK     (1 << 0)
+#define I2C_MST_STATUS_PASS_THROUGH  (0x80)
+#define I2C_MST_STATUS_SLV4_DONE     (0x40)
+#define I2C_MST_STATUS_LOST_ARB      (0x20)
+#define I2C_MST_STATUS_SLV4_NACK     (0x10)
+#define I2C_MST_STATUS_SLV3_NACK     (0x08)
+#define I2C_MST_STATUS_SLV2_NACK     (0x04)
+#define I2C_MST_STATUS_SLV1_NACK     (0x02)
+#define I2C_MST_STATUS_SLV0_NACK     (0x01)
 
 //////////////////////////////////////////////
 //            I2C slave registers
 //////////////////////////////////////////////
 
 // Bitfielfs for slave address register
-#define I2C_SLV_ADDR_RW     (1 << 7)
+#define I2C_SLV_ADDR_RW     (0x80)
 #define I2C_SLV_ADDR_ID(s)  ((s) & 0b1111111)
 #define I2C_SLV_ADDR_MASK   (0b1111111)
 
 // Bitfielfs for slave control register
-#define I2C_SLV_CTRL_EN         (1 << 7)
-#define I2C_SLV_CTRL_BYTE_SW    (1 << 6)
-#define I2C_SLV_CTRL_REG_DIS    (1 << 5)
-#define I2C_SLV_CTRL_GRP        (1 << 4)
+#define I2C_SLV_CTRL_EN         (0x80)
+#define I2C_SLV_CTRL_BYTE_SW    (0x40)
+#define I2C_SLV_CTRL_REG_DIS    (0x20)
+#define I2C_SLV_CTRL_GRP        (0x10)
 #define I2C_SLV_CTRL_LENG(s)    ((s) & 0b1111)
 #define I2C_SLV_CTRL_LENG_MASK  (0b1111)
 
@@ -266,9 +266,9 @@
 #define I2C_SLV4_DO      0x33
 #define I2C_SLV4_DI      0x35
 #define I2C_SLV4_CTRL    0x34
-#define I2C_SLV4_CTRL_EN           (1 << 7)
-#define I2C_SLV4_CTRL_INT_EN       (1 << 6)
-#define I2C_SLV4_CTRL_REG_DIS      (1 << 5)
+#define I2C_SLV4_CTRL_EN           (0x80)
+#define I2C_SLV4_CTRL_INT_EN       (0x40)
+#define I2C_SLV4_CTRL_REG_DIS      (0x20)
 #define I2C_SLV4_CTRL_MST_DLY(s)   ((s) & 0b11111)
 #define I2C_SLV4_CTRL_MST_DLY_MASK (0b11111)
 
